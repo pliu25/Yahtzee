18382cd442e574fd4b4ce73cd703b46fa37979eb
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-12-06 14:00:39 -0500

    update

---
+++ models/Game_Model_starter.py
@@ -0,0 +1,331 @@
+# Pearl Liu #
+import sqlite3
+import random
+from datetime import datetime
+
+class Game:
+    def __init__(self, db_name, table_name):
+        self.db_name =  db_name
+        self.max_safe_id = 9007199254740991 #maximun safe Javascript integer
+        self.table_name = table_name
+    
+    def initialize_table(self):
+        db_connection = sqlite3.connect(self.db_name)
+        cursor = db_connection.cursor()
+        schema=f"""
+                CREATE TABLE {self.table_name} (
+                    id INTEGER PRIMARY KEY UNIQUE,
+                    name TEXT UNIQUE,
+                    created TIMESTAMP,
+                    finished TIMESTAMP
+                );
+                """
+        cursor.execute(f"DROP TABLE IF EXISTS {self.table_name};")
+        results=cursor.execute(schema)
+        db_connection.close()
+    
+    def exists(self, game_name = None, id = None):
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            '''
+            if email:
+                    exists_check = cursor.execute(
+                        f"SELECT * FROM {self.table_name} WHERE email = ?;", (email,)
+                    ).fetchall() 
+            elif not username and not id:
+                    return {"status": "error", "data": "username or id not provided!"}
+            '''   
+
+            if not game_name and not id:
+                return {"status": "error", "data": "game name or id not provided!"}
+
+            #check if name exists
+            if game_name:
+                exists_check = cursor.execute(
+                    f"SELECT * FROM {self.table_name} WHERE name = ?;", (game_name,)
+                ).fetchall()
+                #print("exists_check_username:", exists_check)
+            
+            #check if id exists
+            if id:
+                exists_check = cursor.execute(
+                    f"SELECT * FROM {self.table_name} WHERE id = ?;", (id,)
+                ).fetchall()
+   
+            # check
+            if len(exists_check) > 0:
+                return {"status": "success", "data": True}
+            else:
+                return {"status": "success", "data": False}
+
+        except sqlite3.Error as error:
+            return {"status": "error", "data": error}
+        finally:
+            db_connection.close() 
+    
+    def create(self, game_info):
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            game_id = random.randint(0, self.max_safe_id)
+
+            #print("game_info", game_info)
+            # TODO: check to see if id already exists!! return error 
+            
+            if self.exists(id = game_id)["data"] == True:
+                return {"status":"error",
+                    "data":"error: id already exists"}
+            
+            if self.exists(game_name = game_info["name"])["data"] == True:
+                return {"status": "error",
+                    "data": "error: game name already exists"}
+            #print("exists_check", self.exists(email = game_info["email"])["data"])
+            
+            #validity checks
+            for char in game_info["name"]:
+                if not (char.isalnum() or char == "-" or char == "_"):
+                    return {"status": "error",
+                            "data": "bad game name: game names can only include A-Z, a-z, 0-9, -, _"
+                            }
+
+            created_date = datetime.now()
+
+            game_data = (game_id, game_info["name"], created_date, created_date)
+            cursor.execute(f"INSERT INTO {self.table_name} VALUES (?, ?, ?, ?);", game_data)
+            db_connection.commit()
+
+            return {"status": "success",
+                    "data": self.to_dict(game_data)
+                    }
+        
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        
+        finally:
+            db_connection.close()
+    
+    def get(self, game_name=None, id=None):
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            '''
+                #Insert your code here
+            '''
+
+            if game_name:
+                if self.exists(game_name=game_name)["data"] == False:
+                    return {"status": "error",
+                        "data": "game with this name does not exist!"}
+                
+                fetch_game_name = cursor.execute(
+                    f"SELECT * FROM {self.table_name} WHERE name = ?;", (game_name,)
+                ).fetchone()
+                return {"status": "success",
+                        "data": self.to_dict(fetch_game_name)}
+
+
+            if id: 
+                if self.exists(id=id)["data"] == False:
+                    return {"status": "error",
+                        "data": "game with this id does not exist!"}
+                
+                fetch_id = cursor.execute(
+                    f"SELECT * FROM {self.table_name} WHERE id = ?;", (id,)
+                ).fetchone()
+                return {"status": "success",
+                        "data": self.to_dict(fetch_id)}
+            
+            else:
+                return {"status": "error",
+                        "data": "username or id not provided!"}
+
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+    
+    def get_all(self): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            '''
+                #Insert your code here
+            '''
+            fetch_all_games = cursor.execute(f"SELECT * FROM {self.table_name};").fetchall()
+            all_games = []
+            for game_data in fetch_all_games:
+                all_games.append(self.to_dict(game_data))
+            print("all_users", all_games)
+            return {"status":"success",
+                    "data":all_games}
+        
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def update(self, game_info): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            '''
+                #Insert your code here
+            '''
+            #print("game_info", game_info)
+            if not game_info:
+                return {"status":"error",
+                    "data": "game info not provided!"}
+            #print("get", self.get(id=game_info["id"]))
+            #print("exists", self.exists(id=game_info["id"]))
+            if self.exists(id = game_info["id"])["data"] == True:
+                for column in game_info:
+                    if column != "id":
+                        if(self.get(id=game_info["id"])["data"]["name"] != game_info["name"]):
+                            if self.exists(game_name=game_info["name"])["data"] == False:
+                                cursor.execute(f"UPDATE {self.table_name} SET {column} = ? WHERE id = ?;", (game_info[column], game_info["id"]))
+                                db_connection.commit()
+                            else:
+                                return {"status":"error",
+                                    "data":"game name already exists!"}
+                        else:
+                            cursor.execute(f"UPDATE {self.table_name} SET {column} = ? WHERE id = ?;", (game_info[column], game_info["id"]))
+                            db_connection.commit()
+
+                return {"status":"success",
+                    "data": self.get(id = game_info["id"])["data"]}
+            
+            else:
+                return {"status":"error",
+                    "data": "updated information doesn't exist!"}
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def is_finished(self, game_name):
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            #game_id = random.randint(0, self.max_safe_id)
+
+            # TODO: check to see if id already exists!! return error 
+            #check if created time and finished time is different (update updates this time)
+            
+
+            if game_name: 
+                if self.exists(game_name = game_name)["data"] == False:
+                    return {"status":"error",
+                    "data": "error: game name doesn't exist"}
+                
+                created, finished = cursor.execute(f"SELECT created, finished from {self.table_name} WHERE name = ?", (game_name, )).fetchone()
+                if created != finished: 
+                    return {"status":"success",
+                        "data": True}
+                else:
+                    return {"status":"success",
+                        "data": False}
+            else:
+                return {"status":"error",
+                    "data": "error: game name not provided"}
+             
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        
+        finally:
+            db_connection.close()
+
+    def remove(self, game_name): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            '''
+                #Insert your code here
+            '''
+
+            if (self.exists(game_name=game_name)["data"] == True):
+                remove_user = self.get(game_name=game_name)["data"]
+                cursor.execute(f"DELETE FROM {self.table_name} WHERE name = '{game_name}';")
+                db_connection.commit()
+
+                return {"status":"success",
+                        "data":remove_user}
+            else:
+                return {"status":"error",
+                    "data":"username doesn't exist!"}
+            
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+    
+    def to_dict(self, user_tuple):
+        '''#Utility function which converts the tuple returned from a SQLlite3 database
+           #into a Python dictionary
+        '''
+        user_dict={}
+        if user_tuple:
+            user_dict["id"]=user_tuple[0]
+            user_dict["name"]=user_tuple[1]
+            user_dict["created"]=str(user_tuple[2])
+            user_dict["finished"]=str(user_tuple[3])
+        return user_dict
+    
+if __name__ == '__main__':
+    '''
+    import os
+    print("Current working directory:", os.getcwd())
+    DB_location=f"{os.getcwd()}/yahtzeeDB.db" #f"{os.getcwd()}/Models/yahtzeeDB.db"
+    table_name = "users"
+    #print("DB_location", DB_location)
+    Users = User(DB_location, table_name) 
+    Users.initialize_table()
+
+    user_details={
+        "email":"justin.gohde@trinityschoolnyc.org",
+        "username":"justingohde",
+        "password":"123TriniT"
+    }
+    exists = Users.exists(username = user_details["username"])
+    print("exists", exists)
+    results = Users.create(user_details)
+    print("returned user",results)
+    get_all = Users.get_all()
+    print("get_all", get_all)
+    '''
+    import os
+    print("Current working directory:", os.getcwd())
+    DB_location=f"{os.getcwd()}/yahtzeeDB.db"
+    table_name = "games"
+    
+    Games = Game(DB_location, table_name) 
+    Games.initialize_table()
+
+    game_details={
+        "name":"woohoo",
+        "created":"2024-11-15 16:50:55",
+        "finished":"2024-11-15 16:53:55"
+    }
+    create_check = Games.create(game_details)
+    print("create_check", create_check)
+    exists = Games.exists(game_name = game_details["name"])
+    print("exists", exists)
+    
+    updated_game_details={
+        'name': 'my_new_game_name', 
+        'created': '2024-11-20 08:30:57.431729', 
+        'finished': '2024-11-20 08:30:57.431729'
+    }
+    update_check = Games.create(updated_game_details)
+    print("update_check", update_check)
+    #print("remove", Games.remove("justingohde"))
+    
+
---
+++ models/Scorecard_Model_starter.py
@@ -0,0 +1,315 @@
+# Pearl Liu #
+
+import sqlite3
+import random
+import json
+
+from User_Model_starter import User
+from Game_Model_starter import Game
+
+class Scorecard:
+    def __init__(self, db_name, scorecard_table_name, user_table_name, game_table_name):
+        self.db_name =  db_name
+        self.max_safe_id = 9007199254740991 #maximun safe Javascript integer
+        self.table_name = scorecard_table_name 
+        self.user_table_name = user_table_name
+        self.game_table_name = game_table_name
+    
+    def initialize_table(self):
+        db_connection = sqlite3.connect(self.db_name, )
+        cursor = db_connection.cursor()
+        schema=f"""
+                CREATE TABLE {self.table_name} (
+                    id INTEGER PRIMARY KEY UNIQUE,
+                    game_id INTEGER,
+                    user_id INTEGER,
+                    categories TEXT,
+                    turn_order INTEGER,
+                    name TEXT,
+                    FOREIGN KEY(game_id) REFERENCES {self.game_table_name}(id) ON DELETE CASCADE,
+                    FOREIGN KEY(user_id) REFERENCES {self.user_table_name}(id) ON DELETE CASCADE
+                )
+                """
+        cursor.execute(f"DROP TABLE IF EXISTS {self.table_name};")
+        results=cursor.execute(schema)
+        db_connection.close()
+    
+    def create(self, game_id, user_id, name):
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+            card_id = random.randint(0, self.max_safe_id)
+            
+            #print("user_id", user_id)
+            categories = json.dumps(Scorecard.create_blank_score_info(self))
+            scorecards = cursor.execute(f"SELECT * FROM {self.table_name} WHERE game_id = {game_id};").fetchall()
+            game_count = len(scorecards)
+            turn_order = game_count + 1
+
+            if turn_order > 4:
+                return {"status": "error",
+                    "data": "too many scorecards initiated! 4 max players"
+                    }
+            
+            player_exists = cursor.execute(f"SELECT * FROM {self.table_name} WHERE user_id = {user_id} AND game_id = {game_id};").fetchall()
+
+            if player_exists:
+                return {"status": "error",
+                    "data": "scorecard already initiated for existing player!"
+                    }
+            
+            sc_data = (card_id, game_id, user_id, categories, turn_order, name)
+            #print(sc_data)
+            cursor.execute(f"INSERT INTO {self.table_name} VALUES (?, ?, ?, ?, ?, ?);", sc_data)
+            db_connection.commit()
+
+            return {"status": "success",
+                    "data": self.to_dict(sc_data)
+                    }
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def get(self, name = None, id=None):
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            if name:
+                sc_info = cursor.execute(f"SELECT * FROM {self.table_name} WHERE name = ?;", (name,)).fetchone()
+
+                if sc_info:
+                    return {"status": "success",
+                        "data": self.to_dict(sc_info)}
+                else:
+                    return {"status": "error",
+                        "data": "scorecard not found!"}
+
+
+            if id: 
+                sc_info = cursor.execute(f"SELECT * FROM {self.table_name} WHERE id = ?;", (id,)).fetchone()
+
+                if sc_info:
+                    return {"status": "success",
+                        "data": self.to_dict(sc_info)}
+                else:
+                    return {"status": "error",
+                        "data": "scorecard not found!"}
+            
+            else:
+                return {"status": "error",
+                        "data": "scorecard name or id not provided!"}
+            
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+    
+    def get_all(self): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            fetch_all_cards = cursor.execute(f"SELECT * FROM {self.table_name};").fetchall()
+            sc_list = []
+            for card_data in fetch_all_cards:
+                sc_list.append(self.to_dict(card_data))
+            #print("all_users", all_cards)
+            return {"status":"success",
+                    "data":sc_list}
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+    
+    def get_all_game_scorecards(self, game_name:str): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            all_cards = self.get_all()["data"]
+            print("all_cards", all_cards)
+            sc_list = []
+            for card in all_cards:
+                card_game_name = card["name"].split("|")[0]
+                if card_game_name == game_name:
+                    sc_list.append(card)
+
+            return {"status":"success",
+                    "data":sc_list}
+
+
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def get_all_game_usernames(self, game_name:str): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            all_cards = self.get_all()["data"]
+            usernames = []
+            for card in all_cards:
+                card_game_name = card["name"].split("|")[0]
+                card_username = card["name"].split("|")[1]
+                if card_game_name == game_name:
+                    usernames.append(card_username)
+
+            return {"status":"success",
+                    "data":usernames}
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def get_all_user_game_names(self, username:str): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            all_cards = self.get_all()["data"]
+            game_cards = []
+            for card in all_cards:
+                card_game_name = card["name"].split("|")[0]
+                card_username = card["name"].split("|")[1]
+                if card_username == username:
+                    game_cards.append(card_game_name)
+
+            return {"status":"success",
+                    "data":game_cards}
+            
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def update(self, id, name=None, categories=None): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            scorecards = cursor.execute(f"SELECT * FROM {self.table_name} WHERE id = {id};").fetchall()
+
+            if scorecards:
+                if name:
+                    cursor.execute(f"UPDATE {self.table_name} SET name = ? WHERE id = ?;", (name, id))
+                    db_connection.commit()
+                if categories:
+                    cursor.execute(f"UPDATE {self.table_name} SET categories = ? WHERE id = ?;", (json.dumps(categories), id))
+                    db_connection.commit()
+
+                return {"status":"success",
+                        "data":self.get(id=id)["data"]}
+            else:
+                return {"status":"error",
+                    "data":"scorecard doesn't exist!"}
+
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+
+    def remove(self, id): 
+        try: 
+            db_connection = sqlite3.connect(self.db_name)
+            cursor = db_connection.cursor()
+
+            scorecards = cursor.execute(f"SELECT * FROM {self.table_name} WHERE id = {id};").fetchall()
+
+            if scorecards:
+                remove_card = self.get(id=id)["data"]
+                cursor.execute(f"DELETE FROM {self.table_name} WHERE id = '{id}';")
+                db_connection.commit()
+
+                return {"status":"success",
+                        "data":remove_card}
+            else:
+                return {"status":"error",
+                    "data":"scorecard doesn't exist!"}
+
+
+        except sqlite3.Error as error:
+            return {"status":"error",
+                    "data":error}
+        finally:
+            db_connection.close()
+    
+    def to_dict(self, card_tuple):
+        game_dict={}
+        if card_tuple:
+            game_dict["id"]=card_tuple[0]
+            game_dict["game_id"]=card_tuple[1]
+            game_dict["user_id"]=card_tuple[2]
+            game_dict["categories"]=json.loads(card_tuple[3])
+            game_dict["turn_order"]=card_tuple[4]
+            game_dict["name"]=card_tuple[5]
+        return game_dict
+    
+    def create_blank_score_info(self):
+        return {
+            "dice_rolls":0,
+            "upper":{
+                "ones":-1,
+                "twos":-1,
+                "threes":-1,
+                "fours":-1,
+                "fives":-1,
+                "sixes":-1
+            },
+            "lower":{
+                "three_of_a_kind":-1,
+                "four_of_a_kind":-1,
+                "full_house":-1,
+                "small_straight":-1,
+                "large_straight":-1,
+                "yahtzee":-1,
+                "chance":-1
+            }
+        }
+
+    def tally_score(self, score_info):
+        total_score = 0
+        upper_sum = 0
+        lower_sum = 0
+
+        for value in score_info["upper"]:
+            if score_info["upper"][value] > -1:
+                upper_sum += score_info["upper"][value]
+        if upper_sum >= 63:
+            upper_sum += 35
+        for value in score_info["lower"]:
+            if score_info["lower"][value] > -1:
+                lower_sum += score_info["lower"][value]
+        
+        total_score = upper_sum + lower_sum
+
+        return total_score
+
+if __name__ == '__main__':
+    import os
+    #print("Current working directory:", os.getcwd())
+    DB_location=f"{os.getcwd()}/yahtzeeDB.db"
+    #print("location", DB_location)
+    Users = User(DB_location, "users")
+    Users.initialize_table()
+    Games = Game(DB_location, "games")
+    Games.initialize_table()
+    Scorecards = Scorecard(DB_location, "scorecards", "users", "games")
+    Scorecards.initialize_table()
+
+    print(Scorecards.create_blank_score_info)
