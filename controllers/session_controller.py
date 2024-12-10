369c1d85929d272807cebb46e48b2935adacbaf6
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-12-10 11:39:03 -0500

    update

--- controllers/session_controller.py
+++ controllers/session_controller.py
@@ -2,6 +2,8 @@ from flask import request
 from flask import render_template
 
 def login():
-    # curl "http://127.0.0.1:5000"   
+    # curl "http://127.0.0.1:5000"  
+    query_string = request.query_string
+    print(query_string, "query_string") 
     print(f"request.url={request.url}")
     return render_template('login.html')
--- server.py
+++ server.py
@@ -2,19 +2,29 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import os
+import sys
 import json
 import math
+
+#connect controllers + models 
+fpath = os.path.join(os.path.dirname(__file__), 'controllers')
+sys.path.append(fpath)
+fpath = os.path.join(os.path.dirname(__file__), 'models')
+sys.path.append(fpath)
+from controllers import session_controller, user_controller, game_controller, sc_controller
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def login():
     return render_template("login.html")
+app.add_url_rule('/', view_func = session_controller.login, methods = ['GET'])
+#app.add_url_rule('/index', view_func = session_controller.login, methods = ['GET'])
+app.add_url_rule('/login', view_func = session_controller.login, methods = ['GET'])
 
 @app.route('/game')
 def game():
     username = request.args.get("username")
-    
     return render_template("game.html", username = username)
 
 #console.log sends output to client (inspect page on google)
