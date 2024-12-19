fe156ef13d32b55f973e23b0f398cb6aba65f875
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-12-19 12:46:02 -0500

    update

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -8,7 +8,7 @@ import sys
 fpath = os.path.join(os.path.dirname(__file__), '../models')
 sys.path.append(fpath)
 
-import User_Model 
+from models import User_Model 
 User_DB_location = './models/yahtzeeDb.db'
 User = User_Model.User(User_DB_location, "users")
 
@@ -28,13 +28,19 @@ def user_details():
     print(f"request.url={request.args.get('name')}") #GET request & query string 
     print(f"request.url={request.form.get('name')}") #POST request & form name
 
+    user_info = {
+        "id": "", 
+        "email": "", 
+        "username": "", 
+        "password": ""
+    }
     #get current user details
     if request.method == 'GET':
-        return render_template('user_details.html',request.args.get('username'))
+        return render_template('user_details.html',feedback = "", user_info = user_info)
     
     if request.method == 'POST':
         if User.create()["data"]["status"] != "error":
-            return render_template('user_details.html',request.form.get('username'), request.form.get('password'), request.form.get('email'))
+            return render_template('user_details.html', feedback = "", user_info = user_info)
     '''
     #update user details
     if request.method == 'POST':
