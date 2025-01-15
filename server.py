1f03772937736ea792da2b9b1916b1fbd2248aba
pliu25 <pearl.liu25@trinityschoolnyc.org>
2025-01-15 00:02:53 -0500

    update

--- server.py
+++ server.py
@@ -27,11 +27,15 @@ app.add_url_rule('/login', view_func = session_controller.login, methods = ['GET
 
 #user
 app.add_url_rule('/users', view_func = user_controller.user_details, methods = ['GET','POST'])
-app.add_url_rule('/users', view_func = user_controller.update, methods = ['GET','POST'])
-app.add_url_rule('/users', view_func = user_controller.delete, methods = ['GET','POST'])
+app.add_url_rule('/users/<username>', view_func = user_controller.update, methods = ['GET','POST'])
+app.add_url_rule('/users/delete/<username>', view_func = user_controller.delete, methods = ['GET','POST'])
 
 #game
-
+app.add_url_rule('/games/<username>', view_func = game_controller.get_game, methods = ['GET'])
+app.add_url_rule('/games', view_func = game_controller.create_game, methods = ['POST'])
+app.add_url_rule('/games/join', view_func = game_controller.join_game, methods = ['POST'])
+app.add_url_rule('/games/delete/<game_name>/<username>', view_func = game_controller.remove_user, methods = ['GET'])
+app.add_url_rule('/games/<game_name>/<username>', view_func = game_controller.get_game_play, methods = ['GET'])
 
 '''
 app.add_url_rule('/users/<username>', view_func = user_controller.update, methods = ['POST'])
